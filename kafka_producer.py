0f45cdd49b52cecd23c14f16105b73dc1bc3f788
Ben Gamble <ben.gamble@ably.io>
2021-09-27 13:40:44 +0100

    added back python files

---
+++ kafka_producer.py
@@ -0,0 +1,78 @@
+
+
+import asyncio
+import confluent_kafka
+
+from confluent_kafka.avro import AvroProducer
+from time import time
+from threading import Thread
+
+class AIOProducer:
+    def __init__(self, configs, loop=None, default_key_schema=None,
+                 default_value_schema=None, schema_registry=None):
+        self._loop = loop or asyncio.get_event_loop()
+        self._producer = confluent_kafka.Producer(configs)
+        self._cancelled = False
+        self._poll_thread = Thread(target=self._poll_loop)
+        self._poll_thread.start()
+
+    def _poll_loop(self):
+        while not self._cancelled:
+            self._producer.poll(0.1)
+
+    def close(self):
+        self._cancelled = True
+        self._poll_thread.join()
+
+    def produce(self, topic, value):
+        """
+        An awaitable produce method.
+        """
+        result = self._loop.create_future()
+
+        def ack(err, msg):
+            if err:
+                self._loop.call_soon_threadsafe(result.set_exception, KafkaException(err))
+            else:
+                self._loop.call_soon_threadsafe(result.set_result, msg)
+        self._producer.produce(topic, value, on_delivery=ack)
+        return result
+
+    def produce_acked(self, topic, value, on_delivery):
+        """
+        A produce method in which delivery notifications are made available
+        via both the returned future and on_delivery callback (if specified).
+        """
+        result = self._loop.create_future()
+
+        def ack(err, msg):
+            if err:
+                self._loop.call_soon_threadsafe(
+                    result.set_exception, KafkaException(err))
+            else:
+                self._loop.call_soon_threadsafe(
+                    result.set_result, msg)
+            if on_delivery:
+                self._loop.call_soon_threadsafe(
+                    on_delivery, err, msg)
+        self._producer.produce(topic, value, on_delivery=ack)
+        return result
+
+
+class Producer:
+    def __init__(self, configs):
+        self._producer = confluent_kafka.Producer(configs)
+        self._cancelled = False
+        self._poll_thread = Thread(target=self._poll_loop)
+        self._poll_thread.start()
+
+    def _poll_loop(self):
+        while not self._cancelled:
+            self._producer.poll(0.1)
+
+    def close(self):
+        self._cancelled = True
+        self._poll_thread.join()
+
+    def produce(self, topic, value, on_delivery=None):
+        self._producer.produce(topic, value, on_delivery=on_delivery)
