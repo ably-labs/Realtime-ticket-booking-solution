edce074a6cc1c49991f1b12cfdacaf3e7566d003
Tom Kirby-Green <tomkirbygreen@gmail.com>
2021-10-08 13:31:15 +0100

    Refactored 'app.py'
    Mostly PEP8 stuff around maximum line length and extraneous white-space around named parameters assignment.
    
    Removed unused dependencies.
    
    Noted that the 'request: Request' etc parameters to the 'bookings' etc methods are currently unused. Intentional?

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-
-from fastapi import Body, FastAPI, Request, HTTPException
-from fastapi.staticfiles import StaticFiles 
+from fastapi import FastAPI, Request
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroSerializer
 from confluent_kafka import SerializingProducer
 from pathlib import Path 
 from models import *
-from pydantic import BaseModel, ValidationError, validator
 
 app = FastAPI()
 
@@ -22,11 +19,15 @@ conference_topic = 'conference-topic'
 local_config = {'bootstrap.servers': 'localhost:9092'}
 schema_registry_local = {'url': 'http://127.0.0.1:8081'} 
 
-booking_avro_serializer = AvroSerializer(schema_registry_client = SchemaRegistryClient(schema_registry_local), schema_str = booking_schema)
-conference_avro_serializer   = AvroSerializer(schema_registry_client = SchemaRegistryClient(schema_registry_local), schema_str = conference_schema)
+booking_avro_serializer = AvroSerializer(schema_registry_client=SchemaRegistryClient(schema_registry_local),
+                                         schema_str=booking_schema)
+
+conference_avro_serializer = AvroSerializer(schema_registry_client=SchemaRegistryClient(schema_registry_local),
+                                            schema_str=conference_schema)
                                           
-conference_config   = {'bootstrap.servers': 'localhost:9092', 'value.serializer': conference_avro_serializer}
+conference_config = {'bootstrap.servers': 'localhost:9092', 'value.serializer': conference_avro_serializer}
 booking_config = {'bootstrap.servers': 'localhost:9092', 'value.serializer': booking_avro_serializer}
+
 
 @app.on_event('startup')
 async def startup_event():
@@ -35,23 +36,28 @@ async def startup_event():
     global conference_producer
     conference_producer = SerializingProducer(conference_config)
 
+
 @app.on_event('shutdown')
 def shutdown_event():
     booking_producer.close()
     conference_producer.close()
 
+
 @app.post('/inbound-bookings')
-async def bookings(data: Ably_webhook, request: Request):
+async def bookings(data: AblyWebhook, request: Request):
     try:
         booking_list = [Booking.parse_raw(x.data) for x in data.items[0].data.messages]
         for booking_entry in booking_list:
             booking_producer.produce(booking_topic, None, booking_entry.dict())
+
         booking_producer.poll(0)
         booking_producer.flush
     except Exception as e:
         print(e)
         return 400
+
     return 200
+
 
 @app.post('/inbound-conferences')
 async def conference_creation(data: Conference, request: Request):
@@ -66,8 +72,9 @@ async def conference_creation(data: Conference, request: Request):
     
     return 200
 
+
 @app.post('/presence')
-async def presence_firehose(data: Ably_webhook, request: Request):
+async def presence_firehose(data: AblyWebhook, request: Request):
     # print(data)
     # all presence events
  
